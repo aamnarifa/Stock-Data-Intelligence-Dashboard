40d4a9bac1bb1e4be8d398656b2c3816a21c6c7b
aamnarifa <164160589+aamnarifa@users.noreply.github.com>
2026-04-24 11:31:03 +0530

    Fixed API routes (/api prefix)

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
@@ -10,26 +11,26 @@ const api = axios.create({
 
 // Get list of companies
 export const getCompanies = async () => {
-  const response = await api.get('/stocks/companies');
+  const response = await api.get('/api/stocks/companies');
   return response.data;
 };
 
 // Get stock data
 export const getStockData = async (symbol) => {
-  const response = await api.get(`/stocks/data/${symbol}`);
+  const response = await api.get(`/api/stocks/data/${symbol}`);
   return response.data;
 };
 
 // Get summary
 export const getStockSummary = async (symbol) => {
-  const response = await api.get(`/stocks/summary/${symbol}`);
+  const response = await api.get(`/api/stocks/summary/${symbol}`);
   return response.data;
 };
 
 // Compare stocks
 export const getCompareData = async (symbol1, symbol2) => {
   const response = await api.get(
-    `/stocks/compare?symbol1=${symbol1}&symbol2=${symbol2}`
+    `/api/stocks/compare?symbol1=${symbol1}&symbol2=${symbol2}`
   );
   return response.data;
 };
